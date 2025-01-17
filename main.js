0e5bee25b37307b787303f59ead01e5836e7d11e
Joo Kang <jkang20@students.kennesaw.edu>
2025-01-16 20:32:12 -0500

    Upload Assignment 1

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+    javascript - main.js
+*/
+
+/* link to about page */
+var aboutClick = function() {
+    window.location.href = "/Contents/about.html";
+};
+
+/* link to project page */
+var projectClick = function() {
+    window.location.href = "/Contents/project.html";
+};
+
+/* link to resume page */
+var resumeClick = function() {
+    window.location.href = "/Contents/resume.html";
+};
+
+/* link to contact page */
+var contactClick = function() {
+    window.location.href = "/Contents/contact.html";
+};
+
+/* link back to home page */
+var homeClick = function() {
+    window.location.href = "/index.html";
+}
