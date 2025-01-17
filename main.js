a2ab0748272d045d662de56d72594d81954356fe
Joo Kang <jkang20@students.kennesaw.edu>
2025-01-16 20:41:25 -0500

    main.js - update img click function
    function - updated the link path with "/SWE6813_Assignment_1"

--- main.js
+++ main.js
@@ -4,22 +4,22 @@
 
 /* link to about page */
 var aboutClick = function() {
-    window.location.href = "/Contents/about.html";
+    window.location.href = "/SWE6813_Assignment_1/Contents/about.html";
 };
 
 /* link to project page */
 var projectClick = function() {
-    window.location.href = "/Contents/project.html";
+    window.location.href = "/SWE6813_Assignment_1/Contents/project.html";
 };
 
 /* link to resume page */
 var resumeClick = function() {
-    window.location.href = "/Contents/resume.html";
+    window.location.href = "/SWE6813_Assignment_1/Contents/resume.html";
 };
 
 /* link to contact page */
 var contactClick = function() {
-    window.location.href = "/Contents/contact.html";
+    window.location.href = "/SWE6813_Assignment_1/Contents/contact.html";
 };
 
 /* link back to home page */
